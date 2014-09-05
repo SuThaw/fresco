193a707b89b4d0b9a529e570218c83cd8a2730e0
Su Thaw <suthaw524@gmail.com>
2014-09-06 01:43:14 +0630

    User Controller Email

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -6,6 +6,24 @@
  */
 
 module.exports = {
-	
+	 'create':function(req,res,next){
+                                User.create(req.params.all(),function userCreated(err,user){
+                                        //console.log(user);
+                                       var users = req.params.all();
+
+                                        if(err){
+                                                console.log(err);
+                                                return res.jsonp(err);
+                                        }
+                                        var nodemailer = require('nodemailer');
+                                        var transporter = nodemailer.createTransport();
+										transporter.sendMail({
+    										from: 'suthaw524@gmail.com',
+    										to: user.email,
+    										subject: 'Please Activate Your Account',
+     										html:"To activate your account please click<br><a href='http://128.199.143.11:1337/user/" + user.id +"/activation/" + user.activation_code + "'>http://128.199.143.11:1337/user/" + user.id + "/activation/" + user.activation_code + "</a><br/>Your password is: " + users.generated_password
+									});
+										 res.redirect('/user/' + user.id);
+                                });
 };
 
