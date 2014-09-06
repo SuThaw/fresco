1a3cb0493eda665e1b34731d704b218d00573e2d
Su Thaw <suthaw524@gmail.com>
2014-09-06 12:08:30 +0630

    order system

--- api/models/Orders.js
+++ api/models/Orders.js
@@ -6,6 +6,36 @@
 */
 
 module.exports = {
+	schema:true,
+  attributes: {
+  	address:{
+  		type:'string'
+  	},
+  	addressLat:{
+  		type:'string'
+  	},
+  	addressLng:{
+  		type:'string'
+  	},
+  	township:{
+  		type:'string'
+  	},
+  	items:{
+  		type:'array'
+  	},
+  	date:{
+  		type:'date'
+  	},
+  	status:{
+      type:'int',
+      defaultsTo:0
+    },
+    orderCode:{
+    	type:'string'
+    }
+
+
+  }
 
  };
 
